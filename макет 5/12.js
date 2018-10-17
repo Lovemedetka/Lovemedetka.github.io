a8b6a36f880e4bff7f20915a17e37d4cf0a7305e
Lovemedetka <44239116+Lovemedetka@users.noreply.github.com>
2018-10-17 21:24:15 +0300

    макет5

---
+++ макет 5/12.js
@@ -0,0 +1,8 @@
+function func(){$('.banner').css({'display':'none'});
+$('.product_menu').css({'display':'block'});};
+function func1(){$('.banner').css({'display':'block'});
+$('.product_menu').css({'display':'none'});};
+function func3(){$('.how_do_order').css({'display':"block"});
+$('.first').css({'display':'none'});};
+function func5(){$('.how_do_order').css({'display':"none"});
+$('.first').css({'display':'block'});};
