1d4f9eaaee9dd22e614dcfb38b88e47c9b2cdab8
Lovemedetka <44239116+Lovemedetka@users.noreply.github.com>
2018-12-14 12:38:29 +0300

    Add files via upload

---
+++ макет 8/description_block.js
@@ -0,0 +1,44 @@
+function func(){
+	var elem=document.getElementById('first');
+	var elem1=document.getElementById('rem');
+	var elem2=document.getElementById('block');
+	elem.style.borderRadius=" 20px 20px 0 0";
+	elem.style.right='100';
+	elem.style.bottom='0';
+	elem.style.height='400';
+	elem.style.width='200';
+	elem1.style.transform="rotate(0deg)";
+	elem1.style.marginTop="20";
+	elem1.style.marginLeft="40";
+	elem2.style.width="200";
+	elem2.style.height="50";
+	elem2.style.borderRadius=" 20px 20px 0 0";
+	var elem3=document.getElementById('re');
+	elem3.style.left="150";
+	elem3.style.top="20";
+	var elem4=document.getElementById('last');
+	
+	elem4.style.left="0";
+	elem4.style.bottom="0";
+	
+	
+}
+function func2(){
+	var elem=document.getElementById('first');
+	elem.style.right='-350';
+	elem.style.bottom='200';
+	elem.style.borderRadius="20px 0 0 20px";
+	var elem1=document.getElementById('rem');
+	var elem2=document.getElementById('block');
+	elem2.style.borderRadius="20px 0 0 20px";
+	elem.style.width='400';
+	elem.style.height='200';
+	elem2.style.width="50";
+	elem2.style.height="200";
+	elem1.style.marginTop="100";
+	elem1.style.marginLeft="-25";
+	elem1.style.transform="rotate(-90deg)";
+	var elem4=document.getElementById('last');
+	elem4.style.left="100";
+	elem4.style.bottom="0";
+}
